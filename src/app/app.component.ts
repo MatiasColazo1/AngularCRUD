9f1522d97f175b1e393f50bb19e5f518916a0572
matias <maty_colazo@hotmail.com>
2023-11-14 20:36:50 -0300

    models

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Empleados } from './models/empleados';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +7,9 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'angular-simple-crud';
+  empleadoArray: Empleados[] = [
+    {id: 1, nombre: "Matias", pais: "Argentina"},
+    {id: 2, nombre: "Ryan", pais: "USA"},
+    {id: 3, nombre: "Jorge", pais: "Argentina"}
+  ]
 }
---
+++ src/app/models/empleados.ts
@@ -0,0 +1,11 @@
+export class Empleados {
+    id: number;
+    nombre: string;
+    pais: string;
+
+    constructor(id:number, nombre:string, pais:string) {
+        this.id = id;
+        this.nombre = nombre;
+        this.pais = pais;
+    }
+}
