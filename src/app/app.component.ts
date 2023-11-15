fc65d34cc8875d6e8f604585fd5d24caf1dc4215
matias <maty_colazo@hotmail.com>
2023-11-14 21:47:42 -0300

    ultimos arreglos

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -11,5 +11,27 @@ export class AppComponent {
     {id: 1, nombre: "Matias", pais: "Argentina"},
     {id: 2, nombre: "Ryan", pais: "USA"},
     {id: 3, nombre: "Jorge", pais: "Argentina"}
-  ]
+  ];
+
+  selectEmpleado : Empleados = new Empleados();
+
+  editar(empleado: Empleados) {
+    this.selectEmpleado = empleado;
+  }
+
+  addOrEdit(){
+    if(this.selectEmpleado.id === 0) {
+      this.selectEmpleado.id = this.empleadoArray.length + 1;
+      this.empleadoArray.push(this.selectEmpleado);
+    }
+
+    this.selectEmpleado = new Empleados();
+  }
+
+  eliminar(){
+    if(confirm('Estas seguro de querer eliminarlo?')){
+      this.empleadoArray = this.empleadoArray.filter(x => x != this.selectEmpleado)
+      this.selectEmpleado = new Empleados();
+    }
+  }
 }
--- src/app/models/empleados.ts
+++ src/app/models/empleados.ts
@@ -3,7 +3,7 @@ export class Empleados {
     nombre: string;
     pais: string;
 
-    constructor(id:number, nombre:string, pais:string) {
+    constructor(id: number = 0, nombre: string = '', pais: string = '') {
         this.id = id;
         this.nombre = nombre;
         this.pais = pais;
